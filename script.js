2e876ed3ce5cf4995dca466767cf5a1138b71272
Haddad-Samuel <samuel.yabne@gmail.com>
2024-12-11 14:04:17 +0100

    upt

--- script.js
+++ script.js
@@ -27,3 +27,11 @@ document.getElementById("logout").addEventListener("click", function () {
     window.location.href = "./index.html"; // Redirection vers la page d'accueil (connexion)
 });
 
+async function hashPassword(password) {
+    const encoder = new TextEncoder();
+    const data = encoder.encode(password);
+    const hash = await crypto.subtle.digest("SHA-256", data);
+    return Array.from(new Uint8Array(hash))
+        .map((b) => b.toString(16).padStart(2, "0"))
+        .join("");
+}
